6c11fec100d8f45239578271cde0476e806fb664
eli shabtay <eli.shabtay@gmail.com>
2022-12-26 13:55:22 +0200

    update db

--- database/tms_db.sql
+++ database/tms_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 24, 2022 at 05:40 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: Dec 26, 2022 at 12:54 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,18 +38,20 @@ CREATE TABLE `project_list` (
   `user_ids` text NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `repository` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `project_list`
 --
 
 INSERT INTO `project_list` (`id`, `name`, `description`, `status`, `start_date`, `end_date`, `manager_id`, `user_ids`, `date_created`, `repository`) VALUES
-(1, 'Sample Project', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, metus vitae malesuada mollis, urna nisi luctus ligula, vitae volutpat massa eros eu ligula. Nunc dui metus, iaculis id dolor non, luctus tristique libero. Aenean et sagittis sem. Nulla facilisi. Mauris at placerat augue. Nullam porttitor felis turpis, ac varius eros placerat et. Nunc ut enim scelerisque, porta lacus vitae, viverra justo. Nam mollis turpis nec dolor feugiat, sed bibendum velit placerat. Etiam in hendrerit leo. Nullam mollis lorem massa, sit amet tincidunt dolor lacinia at.&lt;/span&gt;							', 0, '2020-11-03', '2021-01-20', 2, '3,4,5', '2020-12-03 09:56:56', ''),
-(2, 'Sample Project 102', 'Sample Only', 0, '2020-12-02', '2020-12-31', 2, '3', '2020-12-03 13:51:54', ''),
-(3, 'arnolfo rebollosa', 'fghj', 0, '2021-03-23', '2021-03-17', 2, '4', '2021-03-23 14:34:42', ''),
-(4, 'Fenics', '											', 0, '2022-12-01', '2023-01-10', 6, '7', '2022-12-16 22:11:11', ''),
-(5, 'DanceSearch', '						Latin festivals search engines					', 0, '2022-11-01', '2023-02-28', 8, '', '2022-12-22 17:42:14', 'https://github.com/shabtay/dancesearch.git');
+(4, 'Fenics', '																																																																																								', 0, '2022-12-01', '2023-01-10', 8, '', '2022-12-16 22:11:11', ''),
+(5, 'DanceSearch', '																								Latin festivals search engines																				', 0, '2022-11-01', '2023-02-28', 8, '', '2022-12-22 17:42:14', 'https://github.com/shabtay/dancesearch.git'),
+(6, 'TMS', 'TMS', 0, '2022-12-01', '2023-07-25', 8, '', '2022-12-25 17:49:22', 'https://github.com/shabtay/tms.git'),
+(7, 'SARA', '																						', 0, '2023-01-01', '2024-01-01', 8, '', '2022-12-26 10:43:36', ''),
+(8, 'ASP', '																						', 0, '2023-01-01', '2024-01-01', 8, '', '2022-12-26 10:43:54', ''),
+(9, 'Control40', '											', 0, '2023-01-01', '2024-01-01', 8, '', '2022-12-26 10:45:35', ''),
+(10, 'LND/LA Branches', '											', 0, '2023-01-10', '2024-01-01', 8, '', '2022-12-26 10:46:17', '');
 
 -- --------------------------------------------------------
 
@@ -65,14 +67,14 @@ CREATE TABLE `system_settings` (
   `address` text NOT NULL,
   `cover_img` text NOT NULL,
   `repos_folder` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `system_settings`
 --
 
 INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`, `repos_folder`) VALUES
-(1, 'Task Management System', 'info@sample.comm', '+6948 8542 623', '2102  Caldwell Road, Rochester, New York, 14608', '', 'c:\\tmp\\repos');
+(1, 'T.M.S', 'tms@umtb.co.il', '+972524800895', '', '', 'c:\\tmp\\repos');
 
 -- --------------------------------------------------------
 
@@ -87,19 +89,7 @@ CREATE TABLE `task_list` (
   `description` text NOT NULL,
   `status` tinyint(4) NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `task_list`
---
-
-INSERT INTO `task_list` (`id`, `project_id`, `task`, `description`, `status`, `date_created`) VALUES
-(1, 1, 'Sample Task 1', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Fusce ullamcorper mattis semper. Nunc vel risus ipsum. Sed maximus dapibus nisl non laoreet. Pellentesque quis mauris odio. Donec fermentum facilisis odio, sit amet aliquet purus scelerisque eget.&amp;nbsp;&lt;/span&gt;													', 3, '2020-12-03 11:08:58'),
-(2, 1, 'Sample Task 2', 'Sample Task 2							', 1, '2020-12-03 13:50:15'),
-(3, 2, 'Task Test', '								Sample						', 3, '2020-12-03 13:52:25'),
-(4, 2, 'test 23', 'Sample test 23', 1, '2020-12-03 13:52:40'),
-(5, 5, 'bla bla', 'bla bla', 2, '2022-12-22 22:16:46'),
-(6, 5, 'test task', 'another repo test', 2, '2022-12-24 17:50:37');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -116,7 +106,7 @@ CREATE TABLE `users` (
   `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1 = admin, 2 = staff',
   `avatar` text NOT NULL DEFAULT 'no-image-available.png',
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
@@ -124,12 +114,6 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
 (1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, 'no-image-available.png', '2020-11-26 10:57:04'),
-(2, 'Angel Jude', 'Suarez', 'jude@yahoo.com', '202cb962ac59075b964b07152d234b70', 2, '1606978560_avatar.jpg', '2020-12-03 09:26:03'),
-(3, 'Adrian', 'Mercurio', 'adrian@gmail.com', '202cb962ac59075b964b07152d234b70', 3, '1606958760_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 09:26:42'),
-(4, 'Niko', 'Curaza', 'niko@gmail.com', '202cb962ac59075b964b07152d234b70', 3, '1606963560_avatar.jpg', '2020-12-03 10:46:41'),
-(5, 'Adones', 'Evangelista', 'adones@gmail.com', '202cb962ac59075b964b07152d234b70', 3, '1606963620_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 10:47:06'),
-(6, 'manager', 'shabtay', 'mngr@mngr.com', '4297f44b13955235245b2497399d7a93', 2, 'no-image-available.png', '2022-12-16 22:08:08'),
-(7, 'user', 'user', 'user@user.com', '4297f44b13955235245b2497399d7a93', 3, 'no-image-available.png', '2022-12-16 22:10:13'),
 (8, 'Eli', 'Shabtay', 'eli.shabtay@gmail.com', '4390a51d3a4cae89c44c892f9c39eb49', 2, 'no-image-available.png', '2022-12-22 17:41:03');
 
 -- --------------------------------------------------------
@@ -150,23 +134,7 @@ CREATE TABLE `user_productivity` (
   `user_id` int(30) NOT NULL,
   `time_rendered` float NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `user_productivity`
---
-
-INSERT INTO `user_productivity` (`id`, `project_id`, `task_id`, `comment`, `subject`, `date`, `start_time`, `end_time`, `user_id`, `time_rendered`, `date_created`) VALUES
-(1, 1, 1, '							&lt;p&gt;Sample Progress&lt;/p&gt;&lt;ul&gt;&lt;li&gt;Test 1&lt;/li&gt;&lt;li&gt;Test 2&lt;/li&gt;&lt;li&gt;Test 3&lt;/li&gt;&lt;/ul&gt;																			', 'Sample Progress', '2020-12-03', '08:00:00', '10:00:00', 1, 2, '2020-12-03 12:13:28'),
-(2, 1, 1, '							Sample Progress						', 'Sample Progress 2', '2020-12-03', '13:00:00', '14:00:00', 1, 1, '2020-12-03 13:48:28'),
-(3, 1, 2, '							Sample						', 'Test', '2020-12-03', '08:00:00', '09:00:00', 5, 1, '2020-12-03 13:57:22'),
-(4, 1, 2, 'asdasdasd', 'Sample Progress', '2020-12-02', '08:00:00', '10:00:00', 2, 2, '2020-12-03 14:36:30'),
-(5, 2, 3, 'sdfsdfsdfsdfsdfsf', 'sdfsdfs', '2022-12-11', '16:34:00', '23:34:00', 1, 7, '2022-12-18 22:34:17'),
-(6, 5, 5, 'task#5\nadding new sql file export', 'Commit ID#40398057fed8ef8200e99e45f048bc0e2e14771e', '2022-12-24', '10:00:00', '11:00:00', 8, 1, '2022-12-24 17:36:18'),
-(7, 5, 5, 'task#5\nadd app.log to the repo\n    for testing', 'Commit ID#363c918b7bff55ca304aada35ec2eee0fb1650cd', '2022-12-24', '10:00:00', '11:00:00', 8, 1, '2022-12-24 17:38:14'),
-(9, 5, 5, 'task#5\nadd another log for test', 'Commit ID#bc77e268d533f1e824c93df71fc5f9022ca69525', '2022-12-24', '10:00:00', '11:00:00', 8, 1, '2022-12-24 17:44:47'),
-(10, 5, 6, 'task#6 add 20221218 log for testing', 'Commit ID#0a056061f40f6d9ec3a9e2f09858f5021dba0dc7', '2022-12-24', '10:00:00', '11:00:00', 8, 1, '2022-12-24 17:52:17'),
-(11, 5, 5, 'task#5 add 20221219 log to repo', 'Commit ID#7a4bd9ed9b8ebe20551f934131ca61dbd0be3a11', '2022-12-24', '10:00:00', '11:00:00', 8, 1, '2022-12-24 18:39:08');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -210,7 +178,7 @@ ALTER TABLE `user_productivity`
 -- AUTO_INCREMENT for table `project_list`
 --
 ALTER TABLE `project_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `system_settings`
@@ -222,7 +190,7 @@ ALTER TABLE `system_settings`
 -- AUTO_INCREMENT for table `task_list`
 --
 ALTER TABLE `task_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -234,7 +202,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `user_productivity`
 --
 ALTER TABLE `user_productivity`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
